a3750d4d94cb93fa37e53856d94baf22b7581b02
Duckes <mendoza-garcia091998@outlook.com>
2023-02-22 21:41:23 -0600

    that was it

--- TicTacToe/Program.cs
+++ TicTacToe/Program.cs
@@ -5,19 +5,29 @@ int i = 0;
 char currentPlayer = 'O';
 do
 {
-    Console.WriteLine($"Player \"{currentPlayer}\" choose position");
-    t.DrawScenary();
+
+    Drawer();
     int selection = int.Parse(Console.ReadLine());
-    t.Choose(selection,currentPlayer);
-    Console.Clear();
+    t.Choose(selection, currentPlayer);
+    if (t.WhoWon(currentPlayer))
+    {
+        Drawer();
+        break;
+    }
     currentPlayer = WhoPlayer(currentPlayer);
     i++;
 }while(i<9);
-
+Console.WriteLine("Draw/Cat");
 
 char WhoPlayer(char player)
 {
     if (player == 'O')
         return 'X';
     return 'O';
+}
+void Drawer()
+{
+    Console.Clear();
+    Console.WriteLine($"Player \"{currentPlayer}\" choose position");
+    t.DrawScenary();
 }
--- TicTacToe/TicTacToe.cs
+++ TicTacToe/TicTacToe.cs
@@ -14,8 +14,6 @@
             }
             else
                 _ttt[selection - 1] = player;
-
-            WhoWon(player);
         }
 
         private bool IsAlreadyTaken(int selection)
@@ -33,33 +31,34 @@
             Console.WriteLine($" {_ttt[0]} | {_ttt[1]} | {_ttt[2]}");
         }
 
-        private void WhoWon(char player)
+        public bool WhoWon(char player)
         {
             if (_ttt[0] == _ttt[4] && _ttt[0] == _ttt[8] && _ttt[0] != '\0')
             {
                 Console.WriteLine($"Player {player} won");
-                return;
+                return true;
             }
 
-            if (_ttt[2] == _ttt[4] && _ttt[2] == _ttt[6] && _ttt[0] != '\0')
+            if (_ttt[2] == _ttt[4] && _ttt[2] == _ttt[6] && _ttt[2] != '\0')
             {
                 Console.WriteLine($"Player {player} won");
-                return;
+                return true;
             }
-            for (int i = 0,j=0; i <=3; i++,j+=3)
+            for (int i = 0,j=0; i <3; i++,j+=3)
             {
                 if (_ttt[j] == _ttt[j + 1] && _ttt[j] == _ttt[j + 2] && _ttt[j] != '\0')
                 {
                     Console.WriteLine($"Player {player} won");
-                    return;
+                    return true;
                 }
 
                 if (_ttt[i] == _ttt[i + 3] && _ttt[i] == _ttt[i + 6] && _ttt[i] != '\0')
                 {
                     Console.WriteLine($"Player {player} won");
-                    return;
+                    return true;
                 }
             }
+            return false;
         }
     }
 }
