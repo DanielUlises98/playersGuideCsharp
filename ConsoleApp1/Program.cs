ff97315c8e29b322bf365546f2c44a5477abed3f
Duckes <mendoza-garcia091998@outlook.com>
2023-03-04 12:53:32 -0600

    Structures

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,28 @@
+﻿
+
+Coordinate coordinate = new Coordinate(4,4);
+Coordinate coordinate2 = new Coordinate(5,5);
+if (coordinate.AreTheyClose(coordinate2))
+    Console.WriteLine("Coordinates are close");
+public struct Coordinate
+{
+    private readonly int x;
+    private readonly int y;
+
+
+    public Coordinate(int x, int y)
+    {
+        this.x = x;
+        this.y = y;
+    }
+
+    public bool AreTheyClose(Coordinate cordinate)
+    {
+        int absX = Math.Abs(cordinate.x - x);
+        int absY = Math.Abs(cordinate.y - y);
+
+        if (absX == 1 || absX < 1 && absY == 1 || absY < 1)
+            return true;
+        return false;
+    }
+}
