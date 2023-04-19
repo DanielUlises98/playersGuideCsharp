932e93f062be8c6dbbae1f373bad940d2ae4c4b5
Duckes <mendoza-garcia091998@outlook.com>
2023-04-18 21:03:31 -0600

    war preparations

---
+++ WarPreparations/Program.cs
@@ -0,0 +1,29 @@
+﻿
+
+Sword basicSword = new Sword(Material.iron, Gemstone.noGemstone, 1.5f, 2.4f);
+Sword variationSword = basicSword with {mats=Material.wood,lenght=3f};
+Sword variationSword2 = basicSword with { mats = Material.binarium, crossWardWidth = 3f };
+
+Console.WriteLine(basicSword);
+Console.WriteLine(variationSword);
+Console.WriteLine(variationSword2);
+
+record Sword(Material mats, Gemstone gem, float lenght, float crossWardWidth);
+
+enum Material
+{
+    wood,
+    bronze,
+    iron, 
+    steel,
+    binarium
+}
+enum Gemstone 
+{
+    emerald, 
+    amber,
+    sapphire,
+    diamond,
+    bitstone,
+    noGemstone
+}
